ce31dc36a92ec687919a2bc2aacd20d0e0dcf658
shaimatanni <saimatanni60@gmail.com>
2024-10-24 13:13:49 +0600

    add env.local

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,13 +1,13 @@
-import { NextResponse } from 'next/server';
-import axios from 'axios';
+import { NextResponse } from "next/server";
+import axios from "axios";
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const page = searchParams.get('page') || '1';
-  const query = searchParams.get('query') || '';
+  const page = searchParams.get("page") || "1";
+  const query = searchParams.get("query") || "";
 
   const endpoint = query
     ? `${BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${query}&page=${page}`
@@ -17,7 +17,10 @@ export async function GET(request: Request) {
     const response = await axios.get(endpoint);
     return NextResponse.json(response.data);
   } catch (error) {
-    console.log('error', error)
-    return NextResponse.json({ error: 'Failed to fetch movies' }, { status: 500 });
+    console.log("error", error);
+    return NextResponse.json(
+      { error: "Failed to fetch movies" },
+      { status: 500 }
+    );
   }
 }
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,7 @@
-// components/Loader.tsx
 const Loader = () => (
-    <div className="flex justify-center items-center my-8">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
-    </div>
-  );
-  
-  export default Loader;
-  
+  <div className="flex justify-center items-center my-8">
+    <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
+  </div>
+);
+
+export default Loader;
